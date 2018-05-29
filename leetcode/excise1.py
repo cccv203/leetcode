ffc4f2089b9d9c31812a0665c14649fa2eead14e
cccv203 <18840822751@163.com>
2018-05-29 20:27:14 +0800

    version 1.1

--- leetcode/excise1.py
+++ leetcode/excise1.py
@@ -27,5 +27,5 @@ class Solution:
 if __name__=='__main__':
     nums=[0,0,0,1,2,3,3,3,3]
     i = Solution().removeDuplicates(nums)
-    print(nums)
-    print(i)
+    print(nums)    # [0, 1, 2, 3]
+    print(i)       # 4
